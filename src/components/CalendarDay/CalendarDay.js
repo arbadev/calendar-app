1b4b488d25bd974fac32e16c234b02dd60ebac0a
Andres Barradas <a3barradas@gmail.com>
2019-11-03 19:45:45 -0500

    fully completed calendar

--- src/components/CalendarDay/CalendarDay.js
+++ src/components/CalendarDay/CalendarDay.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import EventItem from '../EventCalendarItem';
 
 import styles from './CalendarDay.module.scss';
 
+const RENDER_ROWS = 2;
+
 const CalendarDay = ({ day }) => {
-  const { dayOfMonth } = day;
+  const {
+    dayOfMonth, isToday, isSameMonth, isWeekend,
+  } = day;
   const events = [
     {
       id: 2,
@@ -24,14 +29,49 @@ const CalendarDay = ({ day }) => {
       label: '#ca3e47',
       isMultiDayEvent: false,
     },
+    {
+      id: 4,
+      startDate: '2019-11-03T07:20:07.520Z',
+      endDate: '2019-11-03T07:20:07.520Z',
+      note: 'string string string string',
+      label: '#ca3e47',
+      isMultiDayEvent: false,
+    },
+    {
+      id: 5,
+      startDate: '2019-11-03T07:20:07.520Z',
+      endDate: '2019-11-03T07:20:07.520Z',
+      note: 'string string string string',
+      label: '#ca3e47',
+      isMultiDayEvent: false,
+    },
   ];
+
+  const boxClass = classNames(styles.calendarDay, { [styles.calendarDayWeekend]: isWeekend });
+
+  const dayClass = classNames(
+    styles.calendarDay__day,
+    { [styles.calendarDay__dayToday]: isToday },
+    { [styles.calendarDay__dayAnotherMonth]: !isSameMonth },
+  );
+
   return (
-    <div className={styles.calendarDay}>
-      <p className={styles.calendarDay__day}>{dayOfMonth}</p>
+    <div className={boxClass}>
+      <div className={styles.calendarDay__dayContent}>
+        <p className={dayClass}>{dayOfMonth}</p>
+      </div>
       <div className={styles.calendarDay__events}>
-        {events.map((e) => (
-          <EventItem event={e} key={e.id} />
-        ))}
+        {events.map((e, i) => {
+          if (i < RENDER_ROWS) {
+            return <EventItem event={e} key={e.id} />;
+          }
+          return <></>;
+        })}
+        <p className={styles.calendarDay__extras}>
+          {events.length - 2}
+          {' '}
+more ...
+        </p>
       </div>
     </div>
   );
